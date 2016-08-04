6d69da54c11a1bda5303e8e3622d4d0932dba938
Gary Murphy <compsciguy.it@gmail.com>
2016-08-04 17:44:14 +1000

    block allocating algorithm almost implemented

--- src/game/App.java
+++ src/game/App.java
@@ -1,56 +1,176 @@
 package game;
 
+import java.util.Random;
+
 public class App {
 
-	public static int size = 16;
+	public static int size = 5;
+	public static int num_spaces = size * size;
+	public static boolean dead_end;
 	
 	public static void main(String[] args) {
 
-		Block[] Maze = new Block[size];
+		Block[][] Maze = new Block[size][size];
+		int count = 0;
 		
-		for (int i = 0; i < size; i++) {
-			Maze[i] = new Block(i);
+		for (int block_row = 0; block_row < size; block_row++) {
+			for (int block_col = 0; block_col < size; block_col++) {
+				count++;
+				Maze[block_row][block_col] = new Block(count);				
+			}
 		}
 
 		// increment through each block in the maze
 		// until all are no longer vacant
-		for (Block block : Maze) {
+		do {
+			// reset flag to engage a new tunnel
+			dead_end = false;
 			
-			// check if current block is vacant
-			if (block.isVacant()) {
+			// begin incrementing through spaces in maze
+			for (int block_row = 0; block_row < size; block_row++) {
+				for (int block_col = 0; block_col < size; block_col++) {
 				
-				// setVacant to false
-				block.setVacant(false);
-				
-				// check if adjacent blocks are occupied
-				
-					// if a block is occupied check if that side's value is set to open or closed
-					// set same side in current block the same value
-				
-					// decrement number of sides variable in current block
-				
-				// if number of sides > 0 randomise that number
-				
-					// check each side of current block
-				
-						// if side is unassigned, check if random number is greater than zero
-				
-							// assign that side to be closed
-							// decrement random number
-							// decrement number of sides
-				
-						// if side is unassigned and random number is not greater than zero
-				
-							// assign that side to open
+					// check if current block is vacant
+					if (Maze[block_row][block_col].isVacant()) {
+						
+						// setVacant to false and decrement number of spaces in maze
+						Maze[block_row][block_col].setVacant(false);
+						num_spaces--;
+						
+						/*
+						 * check if adjacent blocks are occupied or a boundary
+						 * if that side's value is set to open or closed
+						 * set same side in current block the same value
+						 * and decrement "number of sides" variable in current block
+						*/
+						
+						// check North block
+						if ((block_row == 0) || Maze[block_row - 1][block_col].getS_side() == Side.CLOSED) {
+							Maze[block_row][block_col].setN_side(Side.CLOSED);
+							Maze[block_row][block_col].setNum_sides(Maze[block_row][block_col].getNum_sides() - 1);
+						}
+						else if (Maze[block_row - 1][block_col].getS_side() == Side.OPEN) {
+							Maze[block_row][block_col].setN_side(Side.OPEN);
+							Maze[block_row][block_col].setNum_sides(Maze[block_row][block_col].getNum_sides() - 1);						
+						}
+						
+						// check South block
+						if ((block_row == size - 1) || Maze[block_row + 1][block_col].getN_side() == Side.CLOSED) {
+							Maze[block_row][block_col].setS_side(Side.CLOSED);
+							Maze[block_row][block_col].setNum_sides(Maze[block_row][block_col].getNum_sides() - 1);
+						}
+						else if (Maze[block_row + 1][block_col].getN_side() == Side.OPEN) {
+							Maze[block_row][block_col].setS_side(Side.OPEN);
+							Maze[block_row][block_col].setNum_sides(Maze[block_row][block_col].getNum_sides() - 1);
+						}
+						
+						// check West block
+						if ((block_col == 0) || Maze[block_row][block_col - 1].getE_side() == Side.CLOSED) {
+							Maze[block_row][block_col].setW_side(Side.CLOSED);
+							Maze[block_row][block_col].setNum_sides(Maze[block_row][block_col].getNum_sides() - 1);
+						}
+						else if (Maze[block_row][block_col - 1].getE_side() == Side.OPEN) {
+							Maze[block_row][block_col].setW_side(Side.OPEN);
+							Maze[block_row][block_col].setNum_sides(Maze[block_row][block_col].getNum_sides() - 1);
+						}
+						
+						// check East block
+						if (block_col == size - 1 || Maze[block_row][block_col + 1].getW_side() == Side.CLOSED) {
+							Maze[block_row][block_col].setE_side(Side.CLOSED);
+							Maze[block_row][block_col].setNum_sides(Maze[block_row][block_col].getNum_sides() - 1);
+						}
+						else if (Maze[block_row][block_col + 1].getW_side() == Side.OPEN) {
+							Maze[block_row][block_col].setE_side(Side.OPEN);
+							Maze[block_row][block_col].setNum_sides(Maze[block_row][block_col].getNum_sides() - 1);
+						}	
+						
+						// if number of sides > 0 randomise that number
+						if (Maze[block_row][block_col].getNum_sides() > 0) {
+							Random rand = new Random();
+							int pathway = rand.nextInt(Maze[block_row][block_col].getNum_sides());
+							
+							// check each side of current block
+							// if side is unassigned and random number is equal to zero, assign side to open and break
+							// otherwise assign side to closed and decrement random number
+							for (int sides = 4; sides > 0; sides--) {
+								if (Maze[block_row][block_col].getN_side() == Side.UNASSIGNED && pathway == 0) {
+									Maze[block_row][block_col].setN_side(Side.OPEN);
+									break;
+								}
+								else {
+									Maze[block_row][block_col].setN_side(Side.CLOSED);
+									pathway--;
+								}
+								
+								if (Maze[block_row][block_col].getE_side() == Side.UNASSIGNED && pathway == 0) {
+									Maze[block_row][block_col].setE_side(Side.OPEN);
+									break;
+								}
+								else {
+									Maze[block_row][block_col].setN_side(Side.CLOSED);
+									pathway--;
+								}
+								
+								if (Maze[block_row][block_col].getS_side() == Side.UNASSIGNED && pathway == 0) {
+									Maze[block_row][block_col].setS_side(Side.OPEN);
+									break;
+								}
+								else {
+									Maze[block_row][block_col].setN_side(Side.CLOSED);
+									pathway--;
+								}
+								
+								if (Maze[block_row][block_col].getW_side() == Side.UNASSIGNED && pathway == 0) {
+									Maze[block_row][block_col].setW_side(Side.OPEN);
+									break;
+								}
+								else {
+									Maze[block_row][block_col].setN_side(Side.CLOSED);
+									pathway--;
+								}
+							}
+
 							// close all unassigned sides
-				
-				/////////////////////////////
-				
-				// Move to next block through side that just been assigned to be open
-				
+							for (int sides = 4; sides > 0; sides--) {
+								if (Maze[block_row][block_col].getN_side() == Side.UNASSIGNED) {
+									Maze[block_row][block_col].setN_side(Side.CLOSED);
+								}
+								if (Maze[block_row][block_col].getE_side() == Side.UNASSIGNED) {
+									Maze[block_row][block_col].setE_side(Side.CLOSED);
+								}
+								if (Maze[block_row][block_col].getS_side() == Side.UNASSIGNED) {
+									Maze[block_row][block_col].setS_side(Side.CLOSED);
+								}
+								if (Maze[block_row][block_col].getW_side() == Side.UNASSIGNED) {
+									Maze[block_row][block_col].setW_side(Side.CLOSED);
+								}
+							}
+						}
+						// else flag dead_end and break
+						else {
+							dead_end = true;
+							break;
+						}
+						/*
+						 * Code breaks at the incorrect moment
+						 * it needs to go to the next space that has been opened
+						 * it should only break when all sides are assigned and only one is open 
+						 */
+						// Move to next block through side that just been assigned to be open
+					}
+				}
+				if (dead_end) {
+					break;
+				}
 			}
+		} while (num_spaces > 0);
+		
+		/*
+		 * Error check
+		for (int block = 0; block < size; block++) {
+			
+			System.out.println(Maze[1][block].getN_side());
 		}
-
+		*/
 	}
-
 }
